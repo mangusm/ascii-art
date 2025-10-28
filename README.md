bf9cfc688303b5edb3d6b622cab354cb163d02ce
mangusm <michaelwmangus@gmail.com>
2025-10-28 19:32:17 -0400

    bolting more crap onto other crap (color support)

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ascii-art
+
+TODO: make this code... good?
+
+```sh
+mkdir images
+wget https://daverupert.com/images/posts/2022/spectrum/color-wheel.png -O images/color_wheel.png
+go run . -f images/rainbow.png -w 100 -i
+go run . -f images/color_wheel.png -w 100 -c -i
+```
--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"image"
 	_ "image/jpeg"
 	_ "image/png"
 	"log"
 	"os"
-	"strconv"
 )
+
+type RGB struct {
+	r uint32
+	g uint32
+	b uint32
+}
 
 // these magic weights are how you convert color to grayscale, apparently
 const rWeight float32 = 0.299
@@ -62,7 +68,7 @@ func getSteps(max int, width int) []int {
 }
 
 // Does exactly what it sounds like
-func getAvgRgbOfChunk(stepsX []int, stepsY []int, ix int, iy int, m image.Image) int {
+func getAvgRgbOfChunk(stepsX []int, stepsY []int, ix int, iy int, m image.Image) (int, RGB) {
 	// calculate the width of the chunk, _dx_
 	startX := 0
 	if ix > 0 {
@@ -79,17 +85,26 @@ func getAvgRgbOfChunk(stepsX []int, stepsY []int, ix int, iy int, m image.Image)
 
 	// Iterate over the pixels in the chunk and calculate their (weighted?) average RGB value
 	sum := 0
+	rgb := RGB{0, 0, 0}
 	for y := startY; y < stepsY[iy]; y++ {
 		for x := startX; x < stepsX[ix]; x++ {
 			r, g, b, _ := m.At(x, y).RGBA()
+			rgb.r += r
+			rgb.g += g
+			rgb.b += b
 			sum += int(rWeight*float32(r) + gWeight*float32(g) + bWeight*float32(b))
 		}
 	}
 
+	numPixels := dx * dy * 255
+	rgb.r /= uint32(numPixels)
+	rgb.g /= uint32(numPixels)
+	rgb.b /= uint32(numPixels)
+
 	// This magic 257 is needed to convert the RGB values to 0-255 values because why not
 	avg := sum / (dx * dy * 257)
 
-	return avg
+	return avg, rgb
 }
 
 // Convert a chunk's average RGB value to a rune
@@ -118,30 +133,37 @@ func avgToChar(avg int, isInverted bool) rune {
 	return '+'
 }
 
-// check if the command line arguments are valid
-func checkArgs(args []string) (string, int, bool) {
-	if len(os.Args) < 2 {
-		panic("A filename must be given")
+// replaced checkArgs with flag-based parser
+func parseFlags() (string, int, bool, bool) {
+	var file string
+	var width int
+	var invert bool
+	var color bool
+
+	flag.StringVar(&file, "file", "", "image file to convert")
+	flag.StringVar(&file, "f", "", "image file to convert (shorthand)")
+	flag.IntVar(&width, "width", 0, "output width in characters")
+	flag.IntVar(&width, "w", 0, "output width in characters (shorthand)")
+	flag.BoolVar(&invert, "invert", false, "invert brightness mapping")
+	flag.BoolVar(&invert, "i", false, "invert brightness mapping (shorthand)")
+	flag.BoolVar(&color, "color", false, "use color output")
+	flag.BoolVar(&color, "c", false, "use color output (shorthand)")
+
+	flag.Parse()
+
+	if file == "" {
+		log.Fatal("A filename must be given (use --file or -f)")
+	}
+	if width <= 0 {
+		log.Fatal("Width must be > 0 (use --width or -w)")
 	}
 
-	// The file to convert to ascii art
-	var fileName = os.Args[1]
-	if len(os.Args) < 3 {
-		panic("A width must be given")
-	}
-
-	// the number of characters wide that the output will be
-	var width, err = strconv.Atoi(os.Args[2])
-	if err != nil {
-		panic(fmt.Sprintf("Invalid width: '%s'", os.Args[2]))
-	}
-	var isInverted bool = len(os.Args) == 4 && os.Args[3] == "invert"
-	return fileName, width, isInverted
+	return file, width, invert, color
 }
 
 func main() {
 	// Check that the command line arguments are valid
-	fileName, width, isInverted := checkArgs(os.Args)
+	fileName, width, isInverted, useColor := parseFlags()
 
 	// Decode the JPEG data
 	reader, err := os.Open(fileName)
@@ -164,25 +186,38 @@ func main() {
 	// Split the image's height into a proportional number of intervals that are as even as possible
 	stepsY := getSteps(bounds.Max.Y, int(float32(width)*float32(bounds.Max.Y)/float32(bounds.Max.X)/2))
 
-	// This is the list of runes that will be appended to as chunks of the image are calculated
-	out := []rune{}
+	// This is the list of outRunes that will be appended to as chunks of the image are calculated
+	outRunes := []rune{}
+	outColors := []RGB{}
 
 	if len(stepsX) > 0 && len(stepsY) > 0 {
 		// Iterate over each chunk, calculate its average RGB value,
 		// convert that to a rune, and add it to the output
 		for iy := range stepsY {
 			for ix := range stepsX {
-				avg := getAvgRgbOfChunk(stepsX, stepsY, ix, iy, m)
+				avg, rgb := getAvgRgbOfChunk(stepsX, stepsY, ix, iy, m)
 				rune := avgToChar(avg, isInverted)
-				out = append(out, rune)
+				outRunes = append(outRunes, rune)
+				// Euclidean Distance in RGB Space
+				// distance = sqrt((R2 - R1)^2 + (G2 - G1)^2 + (B2 - B1)^2)
+				outColors = append(outColors, rgb)
 			}
 		}
 		// Print out each character of the output and a newline every _width_ runes
-		for i := 0; i < len(out); i++ {
+		for i := 0; i < len(outRunes); i++ {
 			if i%(width) == 0 {
 				fmt.Println()
 			}
-			fmt.Print(string(out[i]))
+			r := outColors[i].r
+			g := outColors[i].g
+			b := outColors[i].b
+
+			if useColor {
+				coloredRune := fmt.Sprintf("\033[38;2;%d;%d;%dm%s\033[0m", r, g, b, string(outRunes[i]))
+				fmt.Print(coloredRune)
+				continue
+			}
+			fmt.Print(string(outRunes[i]))
 		}
 		fmt.Println()
 	} else {
